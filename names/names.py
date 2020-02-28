cb367de788ba9eaf3ed868f90305fc6fb5ec9b90
Martin Twum Mensah <martint.mensah@gmail.com>
2020-02-28 10:02:24 +0000

    chore: Complete mvp for names.py

--- names/names.py
+++ names/names.py
@@ -1,6 +1,10 @@
 import time
+from binary_search_tree import BinarySearchTree
+import sys
+sys.path.append('../names')
 
 start_time = time.time()
+# start_time = time.process_time()
 
 f = open('names_1.txt', 'r')
 names_1 = f.read().split("\n")  # List containing 10000 names
@@ -13,14 +17,28 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# initialize and add first value to BinarySearchTree
+bst_names = BinarySearchTree(names_1[0])
+# go through names and insert into tree
+for name in names_1:
+    bst_names.insert(name)
+
+# loop through 2nd names list
+for name in names_2:
+    # check if bst_names contains the name
+    if bst_names.contains(name):
+        # append the duplicated names
+        duplicates.append(name)
+
+""" Old code that takes 22 secs """
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
